6bcf732654ffa7d5efb2ecd22b5d5dc0552a94c1
RedViper234 <84193022+RedViper234@users.noreply.github.com>
2023-10-04 22:34:50 +0200

    Pub Sub e documento

---
+++ Assets/Scripts/PubSub/Base/Publisher.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public static class Publisher
+    {
+        private static Dictionary<ValueType, List<ISubscriber>> _allSubscribers = new Dictionary<ValueType, List<ISubscriber>>();
+
+        public static void Subscribe(ISubscriber subscriber, ValueType messageType)
+        {
+            if (_allSubscribers.ContainsKey(messageType))
+            {
+                _allSubscribers[messageType].Add(subscriber);
+            }
+            else
+            {
+                List<ISubscriber> subscribers = new List<ISubscriber> { subscriber };
+
+                _allSubscribers.Add(messageType, subscribers);
+            }
+        }
+
+        public static void Publish(IMessage message)
+        {
+            ValueType messageType = message as ValueType;
+
+            if (!_allSubscribers.ContainsKey(messageType))
+            {
+                Debug.Log("Messagio di tipo: "+messageType);
+                return;
+            }
+            else
+            {
+                Debug.LogWarning("Contengono la chiave");
+            }
+
+            foreach (ISubscriber subscriber in _allSubscribers[messageType])
+            {
+                subscriber.OnPublish(message);
+            }
+        }
+
+        public static void Unsubscribe(ISubscriber subscriber, ValueType messageType)
+        {
+            if (_allSubscribers.ContainsKey(messageType))
+            {
+                _allSubscribers[messageType].Remove(subscriber);
+            }
+        }
+    }
--- Assets/Scripts/StateMachine/States/Actions/IdleAction.cs
+++ Assets/Scripts/StateMachine/States/Actions/IdleAction.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 
 [CreateAssetMenu(fileName = "Nemici/Actions/IdleAction", menuName = "Nemici/Actions/IdleAction")]
-public class IdleAction : Action
+public class IdleAction : Action, ISubscriber
 {
+    private void OnEnable()
+    {
+        Publisher.Subscribe(this, new MessaggioDiProva());
+    }
     public override void Act(StateMachineController controller)
     {
-        
+        Publisher.Publish(new MessaggioDiProva());
     }
 
     public override void ActOnExitState(StateMachineController controller)
     {
         return;
+    }
+
+    public void OnPublish(IMessage message)
+    {
+        if(message is MessaggioDiProva)
+        {
+            Debug.LogWarning("SEI GAY E USI IL PUB SUB");
+        }
     }
 }
